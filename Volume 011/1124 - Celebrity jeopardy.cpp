e66d5a1e17a52bf13938e6d5835630d31976a34d
MaximBarbe <barbe.maxim@gmail.com>
2025-06-20 16:27:46 -0400

    1124 - Celebrity jeopardy

---
+++ Volume 011/1124 - Celebrity jeopardy.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <string>
+
+
+
+
+int main() {
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0); std::cout.tie(0);
+    
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        std:: cout << line << std::endl;
+    }
+
+}
