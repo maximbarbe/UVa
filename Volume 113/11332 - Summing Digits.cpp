275e42266562cce1195ae75493efdff8af94daee
MaximBarbe <barbe.maxim@gmail.com>
2025-06-20 21:01:10 -0400

    summing digits

--- Volume 113/11332 - Summing Digits.cpp
+++ Volume 113/11332 - Summing Digits.cpp
@@ -13,9 +13,27 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 const double PI = acos(-1);
 
+
+int res(int v) {
+    if (v < 10) return v;
+    int sum = 0;
+    while (v != 0) {
+        sum += v%10;
+        v /= 10;
+    }
+    return res(sum);
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    
+    int n;
+    while (1) {
+        cin >> n;
+        if (n == 0) {
+            break;
+        }
+        cout << res(n) << "\n";
+    }
 
 }
